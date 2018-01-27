57808f80935674edd8af230f72a4ebe1fc8f70f3
Erik Simon <simonerik1996@gmail.com>
2018-01-27 16:41:01 +0100

    Added Project with basic GameFLow

---
+++ Assets/Scripts/CubeController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour {
+
+	[SerializeField]
+	GameController controller;
+
+	[SerializeField]
+	GameObject[] syringePrefabs;
+
+	[SerializeField]
+	Transform syringeSocket;
+
+	Syringe syringe;
+
+	public void OnGrab()
+	{
+		syringe = Instantiate(syringePrefabs[Random.Range(0, syringePrefabs.Length)], syringeSocket).GetComponent<Syringe>();
+	}
+
+	public void OnTake()
+	{
+		if (syringe == null)
+			return;
+
+		controller.TakeSyringe(syringe);
+	}
+
+	public void OnThrow()
+	{
+		Destroy(syringe.gameObject);
+		syringe = null;
+	}
+
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	int score;
+	int syringesTaken;
+	int syringesSkiped;
+	float infectionRate;
+	float highness;
+
+	public void TakeSyringe(Syringe syringe)
+	{
+		if(syringe.clean)
+		{
+			highness = Mathf.Clamp(highness + Random.Range(0.02f, 0.08f), 0f, 1f);
+		}
+		else
+		{
+			infectionRate = Mathf.Clamp(infectionRate + Random.Range(0.1f, .3f), 0f, 1f);
+			highness = Mathf.Clamp(highness + Random.Range(0.02f, 0.08f), 0f, 1f);
+		}
+	}
+
+}
